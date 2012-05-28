8ebc09bfc0b348ebeecf849160609d240e2af85f
Sebastian Kusnier <sebastian@kusnier.net>
2012-05-28 14:30:47 +0200

    Mongo restore requires the mongodb package

--- modules/restore/manifests/mongo.pp
+++ modules/restore/manifests/mongo.pp
@@ -3,5 +3,6 @@ class restore::mongo {
     command   => '/usr/bin/mongorestore --drop /private-backup/backups/mongodb/dump',
     creates   => '/var/lib/mongodb/mxc.ns',
     logoutput => true,
+    require   => Package['mongodb-10gen']
   }
 }
