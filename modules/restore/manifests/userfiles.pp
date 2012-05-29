d84bd1bd8393ed834403217e1d20dc9f075bd871
Sebastian Kusnier <sebastian@kusnier.net>
2012-05-29 23:26:26 +0200

    Restore svn and root files

--- modules/restore/manifests/userfiles.pp
+++ modules/restore/manifests/userfiles.pp
@@ -8,6 +8,20 @@ class restore::userfiles {
     notify    => Service['exim4'],
   }
 
+  exec { 'restore-home-svn':
+    command   => '/usr/bin/rsync --archive -v --delete /private-backup/backups/home/svn /home',
+    onlyif    => '/usr/bin/test ! -d /home/svn',
+    logoutput => true,
+    timeout   => 0,
+  }
+
+  exec { 'restore-home-root':
+    command   => '/usr/bin/rsync --archive -v --delete /private-backup/backups/root/. /root',
+    onlyif    => '/usr/bin/test ! -d /root/devel',
+    logoutput => true,
+    timeout   => 0,
+  }
+
   #  exec { 'seek-dotfiles':
   #    cwd       => '/home/seek',
   #    command   => '/usr/bin/git clone --recursive git://github.com/kusnier/dotfiles.git',
