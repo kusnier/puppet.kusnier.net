f157e41af9ae87f7f31597cf70a7b1898bb56edf
Sebastian Kusnier <sebastian@kusnier.net>
2012-05-30 11:00:59 +0200

    Add options to /etc/mongodb.conf

--- modules/restore/manifests/mongo.pp
+++ modules/restore/manifests/mongo.pp
@@ -1,8 +1,25 @@
 class restore::mongo {
+  exec { "mongo-only-localhost":
+    command => '/bin/echo "bind_ip = 127.0.0.1" >> /etc/mongodb.conf',
+    unless  => '/bin/grep "^bind_ip = 127.0.0.1" /etc/mongodb.conf',
+    require => Package['mongodb-10gen'],
+    before  => Exec['restore-mongo'],
+    notify  => Service['mongodb'],
+  }
+
+  service { 'mongodb':
+    hasstatus  => true,
+    hasrestart => true,
+    enable     => true,
+    ensure     => running,
+  }
+
   exec { "restore-mongo":
     command   => '/usr/bin/mongorestore --drop /private-backup/backups/mongodb/dump',
     creates   => '/var/lib/mongodb/mxc.ns',
     logoutput => true,
-    require   => Package['mongodb-10gen']
+    require   => [Package['mongodb-10gen'], Service['mongodb']],
+    tries     => 5,
+    try_sleep => 2,
   }
 }
