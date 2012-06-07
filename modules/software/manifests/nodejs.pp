24a665356f88861d8f7dfc0a4fb33fd6ef64fdfb
Sebastian Kusnier <sebastian@kusnier.net>
2012-06-07 09:43:21 +0200

    DRY: Move repository addition to a new type

--- modules/software/manifests/nodejs.pp
+++ modules/software/manifests/nodejs.pp
@@ -1,24 +1,6 @@
 class software::nodejs {
-  exec { 'add-nodejs-repo':
-    command     => '/usr/bin/apt-add-repository ppa:chris-lea/node.js',
-    creates     => '/etc/apt/sources.list.d/chris-lea-node.js-lucid.list',
-    require     => Package['python-software-properties'],
+  addrepository { 'ppa:chris-lea/node.js':
+    creates  =>  '/etc/apt/sources.list.d/chris-lea-node.js-lucid.list',
+    packages => ['nodejs', 'npm']
   }
-
-  exec { 'nodejs-apt-get-update-after-add':
-    command     => '/usr/bin/apt-get update',
-    subscribe   => Exec['add-nodejs-repo'],
-    refreshonly => true,
-  }
-
-  package { 'nodejs':
-    ensure  => installed,
-    subscribe => Exec['nodejs-apt-get-update-after-add'],
-  }
-
-  package { 'npm':
-    ensure  => installed,
-    subscribe => Exec['nodejs-apt-get-update-after-add'],
-  }
-
 }
