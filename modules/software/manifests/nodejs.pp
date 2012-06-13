67b14141732a5dbdf1290ebf87f45b1b1f939eb2
Sebastian Kusnier <sebastian@kusnier.net>
2012-06-13 22:56:16 +0200

    Use lsbdistcodename for creates filename

--- modules/software/manifests/nodejs.pp
+++ modules/software/manifests/nodejs.pp
@@ -1,6 +1,6 @@
 class software::nodejs {
   software::addrepository { 'ppa:chris-lea/node.js':
-    creates  =>  '/etc/apt/sources.list.d/chris-lea-node.js-lucid.list',
+    creates  =>  "/etc/apt/sources.list.d/chris-lea-node_js-${lsbdistcodename}.list",
     packages => ['nodejs', 'npm']
   }
 }
