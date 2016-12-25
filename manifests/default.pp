86ebd3d207351d9e15ea34eecde8e84feebe76f1
Sebastian Kusnier <sebastian@kusnier.net>
2016-12-25 21:59:15 +0100

    Remove mongodb

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,9 +1,7 @@
 include system
 include system::users
 include software
-include software::mongo
 include software::mosh
-include restore::mongo
 include restore::apache
 include restore::sshd
 include restore::userfiles
