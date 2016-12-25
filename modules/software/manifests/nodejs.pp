13aa61b0bed8d92c8aa98858d43ae452df3e8560
Sebastian Kusnier <sebastian@kusnier.net>
2016-12-25 19:13:22 +0100

    Remove nodejs

--- modules/software/manifests/nodejs.pp
+++
@@ -1,6 +0,0 @@
-class software::nodejs {
-  software::addrepository { 'ppa:chris-lea/node.js':
-    creates  =>  "/etc/apt/sources.list.d/chris-lea-node_js-${lsbdistcodename}.list",
-    packages => ['nodejs']
-  }
-}
